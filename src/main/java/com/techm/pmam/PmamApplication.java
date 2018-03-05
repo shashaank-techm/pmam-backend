e60aabc3bed27eb4bcc698614a190118e8cabdae
Sai Shashaank V.V <SV00450005@techmahindra.com>
2018-03-05 12:29:28 +0530

    Initial Setup

---
+++ src/main/java/com/techm/pmam/PmamApplication.java
@@ -0,0 +1,12 @@
+package com.techm.pmam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmamApplication.class, args);
+	}
+}
